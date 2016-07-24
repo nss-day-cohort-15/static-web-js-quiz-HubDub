74b167d95969ad4d86a35d3e11e012aaf98ca9f8
Debbie Bourne <dblashrink@hotmail.com>
2016-07-24 18:00:53 -0500

    inputs pass values-corrected input validation process

--- quiz.js
+++ quiz.js
@@ -1,20 +1,53 @@
 var inputHeight = document.getElementById("height");
 var inputSymbol = document.getElementById("symbol");
+var treeOutput = document.getElementById("output");
 
-console.log(height);
-console.log(symbol);
+console.log(height);  //this shows the getElement is working
+console.log(symbol);  //this shows the getElement is working
 
-// var actualHeight = inputHeight.value;
-// var actualSymbol = inputSymbol.value;
+//got the enter buttons working.
+height.addEventListener("keypress", function(event) {
+  console.log(event); //the keypress event works
+  if (event.keyCode === 13) {
+    checkFill ();
+    // buildTree ();  //this does call the function
 
-
-document.getElementById("clickThis").addEventListener("click", function(event) {
-  console.log(event);  //prove the event is firing
-  // console.log(actualHeight);
-  // console.log(actualSymbol);
-  console.log(inputHeight.value);
-  console.log(inputSymbol.value);
+  }
 })
+
+symbol.addEventListener("keypress", function(event) {
+  console.log(event);  //the keypress event works
+  if (event.keyCode === 13) {
+    checkFill ();  //this does call the function
+  }
+})
+
+//got the button working
+document.getElementById("clickThis").addEventListener("click", function(event) {
+  // console.log(event);  //prove the event is firing
+  // console.log(inputHeight.value);  //this worked
+  // console.log(inputSymbol.value);  //this worked
+  checkFill ();  //this works, it calls the function
+})
+
+function checkFill () {   //this checks to make sure both height and symbol have input. it works.
+  var inputHeightValue = inputHeight.value;
+  var inputSymbolValue = inputSymbol.value;
+    if (inputHeightValue < 1) {
+      window.alert("fill out the height");
+    } else if (inputSymbolValue < 1) {
+        window.alert("fill out the symbol");
+    } else buildTree ();
+}
+function buildTree () {
+  console.log("let's build a tree");
+  var inputHeightValue = inputHeight.value;
+  var inputSymbolValue = inputSymbol.value;
+  console.log(inputHeightValue); //this did display value
+  console.log(inputSymbolValue); //this did display value
+
+
+}
 
 // if inputHeight.value === 0 {
 //   alert("Please fill out height of tree")
@@ -35,7 +68,7 @@ document.getElementById("clickThis").addEventListener("click", function(event) {
 
 // if we make the loop go the treeheight number of times and make it increase by 2 each loop, then it can fill an array from the top of the tree down. Then we just need to console.log the array from the last integer to the first making sure to give a break each time. But how to do account for the space?
 
-//   we need an event listener that goes on the click and one that fires on the enter button. click and keypress (how get keypress to link to enter button?)
+
 
 
 
