13df3254155bd65f1ac00a17be5e09e5d2ec7308
Debbie Bourne <dblashrink@hotmail.com>
2016-07-24 22:29:36 -0500

    tree not working yet, but getting closer

--- quiz.js
+++ quiz.js
@@ -4,14 +4,12 @@ var treeOutput = document.getElementById("output");
 
 console.log(height);  //this shows the getElement is working
 console.log(symbol);  //this shows the getElement is working
-
-//got the enter buttons working.
+console.log(output);  //this shows the getElement in working
+//got the enter events working.
 height.addEventListener("keypress", function(event) {
   console.log(event); //the keypress event works
   if (event.keyCode === 13) {
-    checkFill ();
-    // buildTree ();  //this does call the function
-
+    checkFill (); //this does call the function
   }
 })
 
@@ -22,7 +20,7 @@ symbol.addEventListener("keypress", function(event) {
   }
 })
 
-//got the button working
+//got the button event working
 document.getElementById("clickThis").addEventListener("click", function(event) {
   // console.log(event);  //prove the event is firing
   // console.log(inputHeight.value);  //this worked
@@ -30,7 +28,8 @@ document.getElementById("clickThis").addEventListener("click", function(event) {
   checkFill ();  //this works, it calls the function
 })
 
-function checkFill () {   //this checks to make sure both height and symbol have input. it works.
+//once it senses an enter or button event it runs a check to make sure both height and symbol have input.
+function checkFill () {   //this works.
   var inputHeightValue = inputHeight.value;
   var inputSymbolValue = inputSymbol.value;
     if (inputHeightValue < 1) {
@@ -39,45 +38,33 @@ function checkFill () {   //this checks to make sure both height and symbol have
         window.alert("fill out the symbol");
     } else buildTree ();
 }
+
+//once it validates it calls the buildTree function to build the tree
 function buildTree () {
   console.log("let's build a tree");
   var inputHeightValue = inputHeight.value;
   var inputSymbolValue = inputSymbol.value;
+  var finalTreeOutput = treeOutput;
   console.log(inputHeightValue); //this did display value
   console.log(inputSymbolValue); //this did display value
-
-
+  var inputHeightNumber = parseInt(inputHeightValue); //turn numinput into integer
+  console.log(inputHeightNumber); //this works, it's a number now
+  var tree = [];
+  // console.log(tree);  //it shows the blank array
+  for (i = 0; i < inputHeightNumber; i++) {
+    console.log(i);
+    newTree = tree.push(inputSymbolValue);
+    // tree = tree += inputSymbolValue;
+    // tree.push(inputSymbolValue);
+    }
+    treeOutput.innerHTML = "<p> " + newTree + " </p>";
 }
+tree2 (
+  0:
+  )
+//haven't figured it out yet. not accounting for the spaces yet.
 
-// if inputHeight.value === 0 {
-//   alert("Please fill out height of tree")
-// }
-
-
-
-// will need an event listener to know when the form button or enter button pushed
-
-// will need an if statement to decide if both values have been filled out.
-
-
-
-
-// button or enter?
-// if statement for both values > 0 on the number or not null for symbol which is a string.
 // then it loops to build the tree the correct number of layers putting the values in an array that has the number of symbols in each row. Then it console.logs the array out backwards.
 
 // if we make the loop go the treeheight number of times and make it increase by 2 each loop, then it can fill an array from the top of the tree down. Then we just need to console.log the array from the last integer to the first making sure to give a break each time. But how to do account for the space?
 
-
-
-
-
-
-// formValidate () {
-//    if document.forms.namedItem(height > 0) && document.forms.namedItem(symbo !== null) {
-//     // the loop statement that creates the tree
-//   } else {
-    // return to blank form
-//   }
-// }
-// keyup event maybe? the key code for enter button is 13
