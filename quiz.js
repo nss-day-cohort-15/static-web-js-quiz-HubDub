e5a7d5920a0efef1e4ddeac0a780f13e10ac63f3
Debbie Bourne <dblashrink@hotmail.com>
2016-07-25 14:25:29 -0500

    got the tree working

--- quiz.js
+++ quiz.js
@@ -1,20 +1,20 @@
 var inputHeight = document.getElementById("height");
 var inputSymbol = document.getElementById("symbol");
-var treeOutput = document.getElementById("output");
+// var treeOutput = document.getElementById("output");  //don't need this
 
-console.log(height);  //this shows the getElement is working
-console.log(symbol);  //this shows the getElement is working
-console.log(output);  //this shows the getElement in working
+// console.log(height);  //this shows the getElement is working
+// console.log(symbol);  //this shows the getElement is working
+// console.log(output);  //this shows the getElement in working
 //got the enter events working.
 height.addEventListener("keypress", function(event) {
-  console.log(event); //the keypress event works
+  // console.log(event); //the keypress event works
   if (event.keyCode === 13) {
     checkFill (); //this does call the function
   }
 })
 
 symbol.addEventListener("keypress", function(event) {
-  console.log(event);  //the keypress event works
+  // console.log(event);  //the keypress event works
   if (event.keyCode === 13) {
     checkFill ();  //this does call the function
   }
@@ -41,30 +41,30 @@ function checkFill () {   //this works.
 
 //once it validates it calls the buildTree function to build the tree
 function buildTree () {
-  console.log("let's build a tree");
+  // console.log("let's build a tree");
   var inputHeightValue = inputHeight.value;
   var inputSymbolValue = inputSymbol.value;
-  var finalTreeOutput = treeOutput;
-  console.log(inputHeightValue); //this did display value
-  console.log(inputSymbolValue); //this did display value
+  // var finalTreeOutput = treeOutput;
+  // console.log(inputHeightValue); //this did display value
+  // console.log(inputSymbolValue); //this did display value
   var inputHeightNumber = parseInt(inputHeightValue); //turn numinput into integer
-  console.log(inputHeightNumber); //this works, it's a number now
-  var tree = [];
-  // console.log(tree);  //it shows the blank array
+  // console.log(inputHeightNumber); //this works, it's a number now
+
   for (i = 0; i < inputHeightNumber; i++) {
-    console.log(i);
-    newTree = tree.push(inputSymbolValue);
-    // tree = tree += inputSymbolValue;
-    // tree.push(inputSymbolValue);
+    // console.log(i);          //it does log the i
+    // console.log(inputSymbolValue);          //it does log the symbol
+    var countSpace = inputHeightNumber - 1; //create spaces b4 symbol
+    // console.log(countSpace - i);       //test my math on the space
+    var outputSpace =" ".repeat(countSpace - i);  // to create spaces before the symbol
+    var outputSymbol =inputSymbolValue.repeat(1+(2*i)); //to create the tree shape
+    console.log(outputSpace + outputSymbol);
     }
-    treeOutput.innerHTML = "<p> " + newTree + " </p>";
 }
-tree2 (
-  0:
-  )
-//haven't figured it out yet. not accounting for the spaces yet.
 
-// then it loops to build the tree the correct number of layers putting the values in an array that has the number of symbols in each row. Then it console.logs the array out backwards.
-
-// if we make the loop go the treeheight number of times and make it increase by 2 each loop, then it can fill an array from the top of the tree down. Then we just need to console.log the array from the last integer to the first making sure to give a break each time. But how to do account for the space?
-
+//4 tall
+//sss*sss
+//ss***ss
+//s*****s
+//*******
+//
+//I first tried to put the height and symbol data into an array by using the loop, unshift and pushing... the thought that I'd then console log it out backwards but I could not figure that out. then I heard about .repeat and decided to try a different tac. finding the right math to make the repeat work was challanging
